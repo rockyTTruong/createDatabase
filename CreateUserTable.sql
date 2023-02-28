0ab54229fa92da1be165a9f8d2c04e99b49cb722
rockyTTruong <123239747+rockyTTruong@users.noreply.github.com>
2023-02-28 16:47:26 -0600

    Add files via upload

---
+++ CreateUserTable.sql
@@ -0,0 +1,10 @@
+Create Table MusicDatabase.User(
+	ID int PRIMARY KEY,
+    role Enum('Admin', 'Listener', 'Artist') not NULL,
+	username varchar(20) not NULL,
+    password varchar(20) not NULL CHECK (password REGEXP '^(?=.*[a-z])(?=.*[A-Z])(?!.*[^a-zA-Z0-9]).{8,}$'),
+    created_at timestamp DEFAULT current_timestamp,
+    modified_at timestamp ON UPDATE current_timestamp,
+    last_login_at timestamp DEFAULT current_timestamp,
+    is_archived bool DEFAULT false
+);
